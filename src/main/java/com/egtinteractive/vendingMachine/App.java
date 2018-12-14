0c73e8005acc5d593fcfd0e3f7c8cbb2bc8cabe1
Penyo Kolev <penyo.kolev@egt-interactive.com>
2018-12-14 09:33:25 +0200

    Initial commit

---
+++ src/main/java/com/egtinteractive/vendingMachine/App.java
@@ -0,0 +1,13 @@
+package com.egtinteractive.vendingMachine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
